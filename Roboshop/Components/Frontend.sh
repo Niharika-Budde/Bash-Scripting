303ef52c94d627abe1b5365d6065496f08c8965e
Niharika-Budde <niharikabudde36@gmail.com>
2023-08-07 20:46:05 +0200

    Added a information

--- Roboshop/Components/Frontend.sh
+++ Roboshop/Components/Frontend.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-USER_ID=$(id -u)
-
+#USER_ID=$(id -u)
+<<'COMMENT'
 if [ $USER_ID -ne 0 ]; then 
    echo -e "\e[31m script is expected to executed by the root user or with a sudo privilage \e[0m \n \t Example: sudo bash wrapper.sh frontend"
    exit 1
 fi
+COMMENT
 
 echo "configuring frontend"
 yum install ngnix -y
