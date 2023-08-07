65b8869eaa586abfc63d7c39a350e26033675597
Niharika-Budde <niharikabudde36@gmail.com>
2023-08-07 22:39:34 +0200

    Added a information

--- Roboshop/Components/Frontend.sh
+++ Roboshop/Components/Frontend.sh
@@ -44,7 +44,7 @@ stat $?
 
 echo -n "sorting the frontend files: "
 mv frontend-main/* .
-mv static/* 
+mv static/* .
 rm -rf frontend-main README.md          &>>/tmp/frontend.log
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
