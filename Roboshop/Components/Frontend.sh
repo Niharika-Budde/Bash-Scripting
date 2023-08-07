7ccfc5c54fce00f1059fe1bad00355707cc06ba0
Niharika-Budde <niharikabudde36@gmail.com>
2023-08-07 20:13:42 +0200

    Added a information

--- Roboshop/Components/Frontend.sh
+++ Roboshop/Components/Frontend.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-echo "I am frontend"
+echo "configuring frontend"
+yum install ngnix -y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#--> need to install this
+
+# yum install nginx -y
+# systemctl enable nginx
+# systemctl start nginx
