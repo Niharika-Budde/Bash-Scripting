726b7fdf458eaf729f62587f72c060afca90e631
Niharika-Budde <niharikabudde36@gmail.com>
2023-08-07 23:06:25 +0200

    Added a information

--- Roboshop/Components/Frontend.sh
+++ Roboshop/Components/Frontend.sh
@@ -3,15 +3,15 @@
 set -e
 
 USER_ID=$(id -u)
-COMPONENT=$1
+${COMPONENT}=$1
 
 if [ $USER_ID -ne 0 ]; then 
-   echo -e "\e[31m script is expected to executed by the root user or with a sudo privilage \e[0m \n \t Example: sudo bash wrapper.sh COMPONENT"
+   echo -e "\e[31m script is expected to executed by the root user or with a sudo privilage \e[0m \n \t Example: sudo bash wrapper.sh ${COMPONENT}"
    exit 1
 fi
 
 
-echo -e "\e[35m configuring COMPONENT.......!\e[0m \n"
+echo -e "\e[35m configuring ${COMPONENT}.......!\e[0m \n"
 
 stat() {
     if [ $1 -eq 0 ]; then
@@ -22,37 +22,37 @@ fi
 }
 
 echo -n "Installing nginx :"
-yum install nginx -y         &>>/tmp/COMPONENT.log
+yum install nginx -y         &>>/tmp/${COMPONENT}.log
 stat $?
 
 echo -n "starting the nginx:"
-systemctl enable nginx       &>>/tmp/COMPONENT.log
-systemctl start nginx        &>>/tmp/COMPONENT.log
+systemctl enable nginx       &>>/tmp/${COMPONENT}.log
+systemctl start nginx        &>>/tmp/${COMPONENT}.log
 stat $?
 
-echo -n "Downloading the COMPONENT component:"
-curl -s -L -o /tmp/COMPONENT.zip "https://github.com/stans-robot-project/COMPONENT/archive/main.zip"
+echo -n "Downloading the ${COMPONENT} ${COMPONENT}:"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 stat $?
 
-echo -n "clean up the COMPONENT: "
+echo -n "clean up the ${COMPONENT}: "
 cd /usr/share/nginx/html
-rm -rf *                    &>>/tmp/COMPONENT.log
+rm -rf *                    &>>/tmp/${COMPONENT}.log
 stat $?
 
-echo -n "extracting the COMPONENT files: "
-unzip /tmp/COMPONENT.zip     &>>/tmp/COMPONENT.log   
+echo -n "extracting the ${COMPONENT} files: "
+unzip /tmp/${COMPONENT}.zip     &>>/tmp/${COMPONENT}.log   
 stat $?
 
-echo -n "sorting the COMPONENT files: "
-mv COMPONENT-main/* .
+echo -n "sorting the ${COMPONENT} files: "
+mv ${COMPONENT}-main/* .
 mv static/* .
-rm -rf COMPONENT-main README.md          &>>/tmp/COMPONENT.log
+rm -rf ${COMPONENT}-main README.md          &>>/tmp/${COMPONENT}.log
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
-echo -n "Restarting COMPONENT"
-systemctl daemon-reload                 &>>/tmp/COMPONENT.log
-systemctl restart nginx                 &>>/tmp/COMPONENT.log
+echo -n "Restarting ${COMPONENT}"
+systemctl daemon-reload                 &>>/tmp/${COMPONENT}.log
+systemctl restart nginx                 &>>/tmp/${COMPONENT}.log
 stat $?
 
 
