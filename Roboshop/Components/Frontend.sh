b5af8380231f0cfe3a4daddda9338f09b6fedddd
Niharika-Budde <niharikabudde36@gmail.com>
2023-08-07 21:38:06 +0200

    Added a information

--- Roboshop/Components/Frontend.sh
+++ Roboshop/Components/Frontend.sh
@@ -19,8 +19,8 @@ else
 fi
 
 echo -n "starting the nginx:"
-systemctl enable nginx
-systemctl start nginx
+systemctl enable nginx       &>>/tmp/frontend.log
+systemctl start nginx        &>>/tmp/frontend.log
 
 if [ $? -eq 0 ]; then
    echo -e "\e[32m success \e[0m"
